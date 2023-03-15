537044703e90a763a1d639dbbed9edffdfe9445f
Computastar <jason.hunter@computastar.com>
2023-03-15 17:50:05 +0000

    Added react-native-localize npm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import userLocation from './services/User-Location/user-location';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <userLocation/>
       </header>
     </div>
   );
